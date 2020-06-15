49655d1b80fd0a91a2da82e213c0df23259c7bc0
catsinspacesuits <simunro@hotmail.co.uk>
2020-06-15 12:44:39 +0200

    Landing page and contact form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,7 @@
- $(document).ready(function() {
-            $(".menu-icon").on("click", function() {
-                  $("nav ul").toggleClass("showing");
-            });
-      });
-
-      // Scrolling Effect
-
-      $(window).on("scroll", function() {
-            if($(window).scrollTop()) {
-                  $('nav').addClass('black');
-            }
-
-            else {
-                  $('nav').removeClass('black');
-            }
-      })
+$(document).ready(function () {
+$('.navbar-light .dmenu').hover(function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+    }, function () {
+        $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+    });
+});
